a84e913b9f237d8036a0619396762f1fc599c852
Vikas8920 <vikasmishra0306@gmail.com>
2024-10-15 16:47:42 +0530

    first commit

---
+++ new-contact-us.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['btn-submit'])){
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  $message = "Message: " . $_POST['message'];
+
+  // Send email to info@tgcindia.com
+  $to = "info@tgcindia.com, tgcanimation@gmail.com, ranjan354@gmail.com";
+  $subject = "New Inquiry From WEB Design  AD TGC INDIA";
+  $headers = "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+  $mail_body = "Name: $name\nPhone: $phone\nEmail: $email\n\n$message";
+  mail($to, $subject, $mail_body, $headers);
+
+  // Send confirmation email to user
+  $user_subject = "Query Submitted";
+  $user_headers = "From: info@tgcindia.com" . "\r\n" . "Reply-To: info@tgcindia.com" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+  $user_mail_body = "Dear $name,\n\nThank you for submitting your query. We will get back to you soon.\n\nBest regards,\nTGC India";
+  mail($email, $user_subject, $user_mail_body, $user_headers);
+
+  // Redirect to thank you page
+  header('Location:  https://tgcindia.com/thanks');
+
+  // Open a separate page
+  echo "<script>window.open('https://www.tgcindia.com/adwords/graphic-design-course-in-delhi/assets/images/courses/graphic-pro.pdf');
+  </script>";
+}
+
+?>
---
+++ php/demo.php
@@ -0,0 +1,54 @@
+<!-- floating form php -->
+<?php  
+ 
+ if(isset($_POST['submitfloat'])) {
+     
+    $success='';
+    $failed='';
+    
+   $mailto = "info@tgcindia.com, tgcanimation@gmail.com";  //My email address
+   //getting customer data
+   $name = $_POST['name']; //getting customer name
+   $fromEmail = $_POST['email']; //getting customer email
+   $phone = $_POST['tel']; //getting customer Phome number
+   
+   $subject2 = "Confirmation: Message was submitted successfully | TGC Animation and Multimedia"; // For customer confirmation
+ 
+   //Email body I will receive
+   $message = " Name: " . $name . "\n"
+   . "Phone Number: " . $phone . "\n"
+    . "Email: " . $fromEmail . "\n";
+ 
+   
+  $headmsg = "New Enquiry For FIX A DEMO ClASS pop-up form Web design Course in Delhi page from Ad TGC INDIA";
+  
+   
+   //Message for client confirmation
+   $message2 = "Dear" . "\n" . $name . "\n"
+   . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+   . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+   . "Regards," . "\n" . "- TGC";
+ 
+   //Email headers
+   $headers = "From: " . $fromEmail; 
+   $headers2 = "From: " . $mailto; // This will receive client
+   
+   //PHP mailer function
+ 
+    $result1 = mail($mailto,$headmsg, $message, $headers); // This email sent to My address
+    $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+   
+   //  header("Location: form-submit.php",);
+    //Checking if Mails sent successfully
+   
+    if ($result1 && $result2) {
+      $success = "Your Message was sent Successfully!";
+      echo "<script>window.open('https://tgcindia.com/thanks/', '_self')</script>";
+    } else {
+      $failed = "Sorry! Message was not sent, Try again Later.";
+      echo $failed;
+    }
+    
+  }
+    ?> 
+    <!-- floating form php end here -->
